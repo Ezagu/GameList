151853454f895a8a0ef5ab1e136ef4c5069f926f
Ezagu <agustincalpedm@gmail.com>
2025-01-09 00:42:13 -0300

    modify_and_delete

--- db_managment.py
+++ db_managment.py
@@ -10,7 +10,7 @@ class DbManagment:
         """Return all values of the table games"""
 
         query = f'''
-        SELECT title, date, score, console, note FROM games ORDER BY {orderby} {order}
+        SELECT title, date, console, score, note FROM games ORDER BY {orderby} {order}
         '''
 
         try:
@@ -22,7 +22,7 @@ class DbManagment:
         except sqlite3.Error:
             print("Error con sqlite")
 
-    def insert_to_db(self, title, date, score, console, note, userid = 1):
+    def insert_to_db(self, title, date, console, score, note, userid = 1):
         query = '''
         INSERT INTO games (title,date,score,console,note,user_id) VALUES(?,?,?,?,?,?)
         '''
@@ -41,6 +41,18 @@ class DbManagment:
         try:
             cursor = self.conn.cursor()
             cursor.execute(query,(title,))
+            self.conn.commit()
+            cursor.close()
+        except sqlite3.Error:
+            print("Error con sqlite")
+
+    def update_game(self, old_title, new_title, new_date, new_console, new_score, new_note):
+        query='''
+        UPDATE games SET title = ?, date = ?, console = ?, score = ?, note = ? WHERE title = ?
+        '''
+        try:
+            cursor = self.conn.cursor()
+            cursor.execute(query,(new_title, new_date, new_console, new_score, new_note, old_title))
             self.conn.commit()
             cursor.close()
         except sqlite3.Error:
--- gui.py
+++ gui.py
@@ -11,7 +11,7 @@ class Gui:
         self.user_id = 1
 
         self.orderby = "title"
-        self.order = "DESC"
+        self.order = "ASC"
 
         self.login_root = None
         self.main_root = None
@@ -37,15 +37,32 @@ class Gui:
         add_button = tk.Button(top_frame, text="+ADD", fg="white", bg="green", command=self.start_add_window)
         add_button.pack(padx=8,pady=8,side="left")
 
-        filter_combobox = ttk.Combobox(top_frame, values=["Título", "Fecha", "Consola", "Puntaje"])
+        def change_filter_mode(events):
+            option = filter_combobox.get()
+            values = {
+                "Título":"title",
+                "Fecha":"date",
+                "Consola":"console",
+                "Puntaje":"score"
+            }
+            self.orderby = values[option]
+            self.update_tree()
+
+        filter_combobox = ttk.Combobox(top_frame, values=["Título", "Fecha", "Consola", "Puntaje"], state="readonly")
         filter_combobox.set("Título")
         filter_combobox.pack(padx=8,pady=8,side="right")
-        #filter_combobox.bind("<<ComboboxSelected>>", pass)
+        filter_combobox.bind("<<ComboboxSelected>>", change_filter_mode)
 
-        asc_desc_button = tk.Button(top_frame, text="ASC", bg="lightgray")
+        def toggle_order():
+            self.order = "ASC" if self.order == "DESC" else "DESC"
+            asc_desc_button.config(text=self.order)
+            self.update_tree()
+
+        asc_desc_button = tk.Button(top_frame, text="ASC", bg="lightgray", command=toggle_order)
         asc_desc_button.pack(padx=5,pady=8, side="right")
 
         self.tree = ttk.Treeview(self.main_root)
+        self.tree.bind("<Double-1>", lambda x :self.start_add_window(True))
 
         self.tree["columns"] = ("title", "date", "console", "score", "note")
         self.tree.column("#0",width=50 ,minwidth=50, anchor="w")  # Columna principal (la que no se nombra)
@@ -68,29 +85,7 @@ class Gui:
 
         self.main_root.mainloop()
 
-    def insert_to_tree(self, title, date, console, score, note):
-        """Insert an item in the tree"""
-        idx = str(self.count_in_tree)
-        self.tree.insert("", "end", idx, text=idx, values=(title, date, console,score,note))
-        self.count_in_tree += 1
-
-    def add_game(self):
-        """Add a game automatically to db"""
-        title = self.title_entry.get()
-        date = self.date_entry.get()
-        score = self.score_entry.get()
-        console = self.console_entry.get()
-        note = self.note_text.get("1.0", "end")
-        self.db.insert_to_db(title, date, score,console,note, self.user_id)
-        self.update_tree()
-
-    def clear_tree(self):
-        """Clear all the items in the tree"""
-        for i  in range(self.count_in_tree - 1):
-            self.tree.delete(str(i + 1))
-        self.count_in_tree = 1
-
-    def start_add_window(self):
+    def start_add_window(self, modify:bool = False):
         root = tk.Toplevel(self.main_root)
         
         title_label = tk.Label(root,text="Título:")
@@ -118,17 +113,95 @@ class Gui:
         self.note_text = tk.Text(root,height=5,width=35)
         self.note_text.grid(row=7, column=0, columnspan=2, sticky="swen",padx=10,pady=5)
 
-        cancel_button = tk.Button(root,text="Cancelar", fg="white", bg="red",width=15,command=lambda:root.destroy())
-        cancel_button.grid(row=8,column=0, pady=10)
+        if not modify:
+            #Si es para añadir un juego
+            cancel_button = tk.Button(root,text="Cancelar", fg="white", bg="red",width=15,command=lambda:root.destroy())
+            cancel_button.grid(row=8,column=0, pady=10)
 
-        accept_button = tk.Button(root, text="Añadir", fg="white", bg="green",width=15,command=lambda : add())
-        accept_button.grid(row=8, column=1, pady=10)
+            accept_button = tk.Button(root, text="Añadir", fg="white", bg="green",width=15,command=lambda : add())
+            accept_button.grid(row=8, column=1, pady=10)
 
-        def add():
-            self.add_game()
-            root.destroy()
+            def add():
+                self.add_game()
+                root.destroy()
+        else:
+            #Para modificar un juego
+            idx = self.tree.selection()
+            data = self.tree.item(idx[0],"values")
+            title = data[0]
+            date = data[1]
+            console = data[2]
+            score = data[3]
+            note = data[4]
 
-        
+            self.title_entry.insert(0,title)
+            self.date_entry.set_date(date)
+            self.console_entry.insert(0,console)
+            self.score_entry.insert(0,score)
+            self.note_text.insert("1.0",note)
+
+            frame_buttons = tk.Frame(root)
+            frame_buttons.grid(row=8, column=0, columnspan=2, padx=10)
+
+            cancel_button = tk.Button(frame_buttons,text="Cancelar", fg="white", bg="red",width=15,command=lambda:root.destroy())
+            cancel_button.grid(row=0,column=0, pady=10, padx=5)
+
+            def eliminate():
+                self.delete_in_db(title)
+                root.destroy()
+                self.update_tree()
+
+            delete_button = tk.Button(frame_buttons,text="Eliminar", fg="white", bg="red",width=15,command=eliminate)
+            delete_button.grid(row=0,column=1, pady=10, padx=5)
+
+            def update():
+                data = self.get_data_entry()
+                self.db.update_game(old_title=title, new_title=data[0],
+                                     new_date=data[1], new_console=data[2], 
+                                     new_score=data[3], new_note=data[4])
+                root.destroy()
+                self.update_tree()
+
+
+            save_button = tk.Button(frame_buttons,text="Guardar", fg="white", bg="green",width=15,command=update)
+            save_button.grid(row=0,column=2, pady=10, padx=5)
+
+
+
+    def delete_in_db(self,title):
+        """Delete an element in db with the title"""
+        self.db.eliminate_game_in_db(title)
+
+    def insert_to_tree(self, title, date, console, score, note):
+        """Insert an item in the tree"""
+        idx = str(self.count_in_tree)
+        self.tree.insert("", "end", idx, text=idx, values=(title, date, console,score,note))
+        self.count_in_tree += 1
+
+    def get_data_entry(self):
+        """Return the data from the entrys (title, date, console, score, note)"""
+        t = self.title_entry.get()
+        d = self.date_entry.get()
+        c = self.console_entry.get()
+        s = self.score_entry.get()
+        n = self.note_text.get("1.0", "end")
+        return (t,d,c,s,n)
+
+    def add_game(self):
+        """Add a game automatically to db"""
+        data = self.get_data_entry()
+        self.db.insert_to_db(data[0], data[1], data[2],data[3],data[4], self.user_id)
+        self.update_tree()
+
+    def delete_tree_item(self, idx):
+        """delete an item in the tree with the index"""
+        self.tree.delete(idx)
+
+    def clear_tree(self):
+        """Clear all the items in the tree"""
+        for i  in range(self.count_in_tree - 1):
+            self.delete_tree_item(str(i+1))
+        self.count_in_tree = 1
 
     def update_tree(self):
         """Update the tree with all the games in db"""
